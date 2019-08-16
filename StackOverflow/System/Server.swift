045fec261d2bc7a7f6816ddac187dee6c6d75805
Joe Edwards <Joe.Edwards.SAV@gmail.com>
2019-08-15 20:32:52 -0400

    adds rough server struct

---
+++ StackOverflow/System/Server.swift
@@ -0,0 +1,18 @@
+//
+//  Server.swift
+//  StackOverflow
+//
+//  Created by Joe Edwards IV on 8/15/19.
+//  Copyright © 2019 Joe Edwards IV. All rights reserved.
+//
+
+import Foundation
+
+struct Server {
+	
+	let host: URL
+	
+	let appKey: String = "tMUPxsnkyL2ZT58jWOWUag(("
+	let clientID: Int = 16001
+	
+}
